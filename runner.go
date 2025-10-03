0245b4280482f4ea45100e78ba9419ac5e56c257
Tiago Angelo <kurtis.angelo@gmail.com>
2025-10-03 17:20:03 -0300

    Add Runner struct and Run method in runner.go

---
+++ runner.go
@@ -0,0 +1,13 @@
+package crd2client
+
+import (
+	"context"
+	"errors"
+)
+
+type Runner struct{}
+
+func (r *Runner) Run(ctx context.Context) error {
+	// TODO: implement the main logic here.
+	return errors.New("not implemented")
+}
